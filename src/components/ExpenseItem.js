723918073eadda3ab02aac99fa341645046457b5
Cristian <crisluna@Mac-mini-de-Cris.local>
2022-01-23 15:37:21 -0600

    Update on tutorial

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
-function ExpenseItem() {
-    return (
-        <div>
-            <div>March 28th</div>
-            <div>
-                <h2>Car Insurance</h2>
-                <div>$294.67</div>
-            </div>
-        </div>
-    );
+import "./ExpenseItem.css";
+import Card from './Card';
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
