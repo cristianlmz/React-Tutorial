936152e1ff2ac9df47aa96f881015ce67f8b7834
Cristian <crislunamz@gmail.com>
2022-02-16 17:52:24 -0600

    update on tutorial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -28,9 +29,14 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    // console.log(`In App.js`, expense);
+    expenses.push(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddNewExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
